9e77a821b04792d5a1a752fcb37f2a974fb70285
Deivison Reis <deivisomreis@bol.com.br>
2016-09-28 15:09:47 -0300

    Feitas algumas configurações, para ativar o SpringMVC

--- src/main/java/com/escolafamilia/configapp/SpringAppConfig.java
+++ src/main/java/com/escolafamilia/configapp/SpringAppConfig.java
@@ -1,5 +1,36 @@
 package com.escolafamilia.configapp;
 
-public class SpringAppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages={"com.escolafamilia.view"})
+public class SpringAppConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//registry.addInterceptor(new ControlAccessPage());
+	}
 }
